b8e96f47ee70767ebbecaa2c621815d3ef69ef9a
mahidhargudipudi <119840056+mahidhargudipudi@users.noreply.github.com>
2023-06-02 00:07:46 +0530

    Add files via upload

---
+++ AVL_tree.c
@@ -0,0 +1,17 @@
+// AVL TREE 
+// Diff between depth of left - depth of right 
+
+/*
+
+ rotations :
+	left rotation 
+	right rotation
+ 
+ 4 cases :
+
+ Right Right case  --> Left rotation 
+ Left Left case    --> Right rotation
+ Right left case   --> Right rotation and left rotation 
+ Left Right case   --> Left rotation and Right rotation
+ 
+ */
